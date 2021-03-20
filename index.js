4b8947e34474233275aade974d455c5319c33a0a
DashNothing <stokic.toni@gmail.com>
2021-03-20 19:05:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Gradient in hero
+var multiple = new Multiple({
+  selector: ".bubble",
+  background: `linear-gradient(
+    72.44deg,
+    #8b6aeb 0%,
+    #3eabf6 60.94%,
+    #67f1ed 100%
+  )`,
+});
