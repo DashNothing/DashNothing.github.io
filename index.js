09611d64949108d1665b549ede62ac8aea8304c2
DashNothing <stokic.toni@gmail.com>
2021-03-22 18:32:03 +0100

    Mobile responsivness added

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-// Gradient in hero
-var multiple = new Multiple({
-  selector: ".bubble",
-  background: `linear-gradient(
-    72.44deg,
-    #8b6aeb 0%,
-    #3eabf6 60.94%,
-    #67f1ed 100%
-  )`,
+// Open/close mobile nav
+
+const nav = document.getElementById("nav");
+const hamburger = document.getElementById("hamburger");
+const closeMenuButton = document.getElementById("close-menu");
+const navLinks = document.querySelectorAll("#nav a");
+
+hamburger.addEventListener("click", () => {
+	toggleMenu();
 });
+
+closeMenuButton.addEventListener("click", () => {
+	toggleMenu();
+});
+
+navLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		closeMenu();
+	});
+});
+
+const openMenu = () => {
+	nav.classList.add("showing");
+	document.body.classList.add("no-scroll");
+};
+
+const closeMenu = () => {
+	nav.classList.remove("showing");
+	document.body.classList.remove("no-scroll");
+};
+
+const toggleMenu = () => {
+	if (nav.classList.contains("showing")) {
+		closeMenu();
+	} else {
+		openMenu();
+	}
+};
